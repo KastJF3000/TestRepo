ea214d2b3c9634a312fcc9737e266ff8a8e57cca
Jonathan <Jonathan@192.168.20.193>
2014-07-25 00:27:37 -0400

    Added Question

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -1,7 +1,11 @@
 public class TestClass {
 	public static void main(String[] args) {
+		
+		TestSubject newGuy = new TestSubject();
+	
 		System.out.println("Hello, World!");
 		System.out.println("Smile");
+		System.out.println("What's Up?");
 		System.out.println("Goodbye, World!");
 	}
 }
